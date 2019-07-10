be44cc24a91c5752f72d192b45d0371bdf53f027
rob jahn <robjahn@gmail.com>
2019-07-10 15:48:11 -0400

    add problem issues

--- src/main/java/com/ewolff/microservice/order/logic/OrderController.java
+++ src/main/java/com/ewolff/microservice/order/logic/OrderController.java
@@ -1,6 +1,14 @@
 package com.ewolff.microservice.order.logic;
 
 import java.util.Collection;
+import java.util.Random;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.Calendar;
+import java.util.Date; 
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -16,9 +24,6 @@ import com.ewolff.microservice.order.clients.Customer;
 import com.ewolff.microservice.order.clients.CustomerClient;
 import com.ewolff.microservice.order.clients.Item;
 
-import java.util.Calendar;
-import java.util.Date; 
-
 import java.io.*;
 
 @Controller
@@ -30,6 +35,11 @@ class OrderController {
 
 	private CustomerClient customerClient;
 	private CatalogClient catalogClient;
+	private final Logger log = LoggerFactory.getLogger(OrderController.class);
+	private static Random rand = new Random();
+
+	// @Value("${app.usedummydata:true}")
+	private boolean useDummyData = false;
 
 	@Autowired
 	private OrderController(OrderService orderService,
@@ -40,16 +50,46 @@ class OrderController {
 		this.customerClient = customerClient;
 		this.catalogClient = catalogClient;
 		this.orderService = orderService;
+
+		log.trace("Dummy Data: {} ", Boolean.toString(useDummyData));
+		System.out.println("Dummy Data: " + Boolean.toString(useDummyData));
 	}
 
 	@ModelAttribute("items")
 	public Collection<Item> items() {
+		if(useDummyData) {
+			ArrayList <Item>items = new ArrayList<Item>();
+			items.add(new Item(1, "Item 1", 100.00));
+			return items;	
+		}
+
 		return catalogClient.findAll();
 	}
 
 	@ModelAttribute("customers")
 	public Collection<Customer> customers() {
-		return customerClient.findAll();
+		if(useDummyData) {
+			Collection<Customer> allCustomers = new ArrayList<Customer>();
+			allCustomers.add(new Customer(1, "Andi", "Grabner", "agrabner@dynatrace.com", "freistaedterstrasse", "Linz"));
+			return allCustomers;
+		}
+
+		Collection<Customer> allCustomers = customerClient.findAll();
+
+		// ************************************************
+		// N+1 Problem
+		// Add additional lookups for each customer
+		// this will cause additional SQL calls
+		// ************************************************
+		Iterator<Customer> itr = allCustomers.iterator();
+		while (itr.hasNext()) {
+			Customer cust = itr.next();
+			long id = cust.getCustomerId();
+			for(int i=1; i<=20; i++){
+				customerClient.getOne(id);
+			}
+		}
+		return allCustomers;
 	}
 
 	@RequestMapping("/")
@@ -65,6 +105,17 @@ class OrderController {
 
 	@RequestMapping(value = "/line", method = RequestMethod.POST)
 	public ModelAndView addLine(Order order) {
+		// ************************************************
+		// in 50% of the cases will return incorrect data
+		// back resulting in a 500 error in the UI
+		// ************************************************
+		int n = rand.nextInt(100);
+		System.out.println("Random Number: " + Integer.toString(n));
+		if(n < 50) {
+			log.trace("ADDING A NULL FOR ORDER!!");
+			System.out.println("ADDING A NULL FOR ORDER!!");
+			return new ModelAndView("orderForm", "order", null);
+		}
 		order.addLine(0, catalogClient.findAll().iterator().next().getItemId());
 		return new ModelAndView("orderForm", "order", order);
 	}
